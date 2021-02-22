82404d0a42c644b1f152bfe8826d27438f94ce49
JarHorner <jhorn384@mtroyal.ca>
2021-02-22 14:24:52 -0700

    adding files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# COMP3512-Assign1
+# COMP 3512 (Winter 2021)
+### Assignment #1: Single-Page App
+
+**Please view `COMP3512 Assignment 1.pdf` for instructions**
+
+  
---
+++ js/index-js.js
@@ -0,0 +1,4 @@
+
+const companyList = 'https://www.randyconnolly.com/funwebdev/3rd/api/stocks/companies.php';
+//remember for stockData, add the symbol # at the end of the url (symbol=) using a queryString.
+const stockData = 'https://www.randyconnolly.com/funwebdev/3rd/api/stocks/history.php?symbol=';
