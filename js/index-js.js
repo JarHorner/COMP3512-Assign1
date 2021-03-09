41519d267b9f01a6338011ca5b2328d80a53bd34
JarHorner <jhorn384@mtroyal.ca>
2021-03-09 00:03:15 -0700

    changes

--- js/index-js.js
+++ js/index-js.js
@@ -75,8 +75,6 @@ function displayInformation(e) {
     populateCompanyInformation(company)
     populateMap(company);
     populateStockData(company);
-    populateDescription(company);
-    populateFinancials(company);
 }
 
 /* Empties the company list, then re-creates it along  with changing all
@@ -91,7 +89,6 @@ function clearButton() {
     document.querySelector('#location').style.display = 'none';
     document.querySelector('#company-information').style.display = 'none';
     document.querySelector('#stocktable').style.display = "none";
-    document.querySelector('.chartsBtn').style.display = "none";
     document.querySelector('.calculations').style.display = "none";
 }
 
@@ -166,9 +163,10 @@ function populateStockData(company) {
         })
         .then((data) => {
             companyStock = data;
+            const table = document.querySelector('table.data');
             const stockdata = document.querySelector("table.data tbody");
             const stockHeaders = document.querySelector("table.data thead tr");
-            stockdata.innerHTML = '';
+	    stockdata.innerHTML = '';
             companyStock.forEach((item) => {
                 const tr = document.createElement('tr');
                 const tdDate = document.createElement('td');
@@ -194,7 +192,6 @@ function populateStockData(company) {
             stockHeaders.addEventListener('click', organizeData);
             document.querySelector('.roller2').style.display = "none";
             document.querySelector('#stocktable').style.display = "block";
-            document.querySelector('.chartsBtn').style.display = "block";
             //calculates each row average, min, and max (refer to each method)
             calculatingAverage();
             calculatingMin();
@@ -265,69 +262,58 @@ function columnNumber(name) {
     switch(name) {
         case 'Date':
             return 0;
-        case 'Open($)':
+        case 'Open':
             return 1;
-        case 'Close($)':
+        case 'Close':
             return 2;
-        case 'Low($)':
+        case 'Low':
             return 3;
-        case 'High($)':
+        case 'High':
             return 4;
         case 'Volume':
             return 5;
     }
 }
 
-/* loops through each row of stock data and creates an average for each column */
 function calculatingAverage() {
     let  x, average, n;
     const table = document.querySelector("table.data");
-    const rows  =  table.rows;
     const averageRow  =  document.querySelector("table.calculations #average-row");
     averageRow.innerHTML =  "";
-    //adds Title of row to front
     let td =  document.createElement('td');
     td.innerHTML  = "<b>Average:<b>";
     averageRow.appendChild(td);
-    //adds Title of row to front
+    const rows  =  table.rows;
+
     for (let c = 1;  c  < 6;  c++)  {
         n  =  0;
         average  = 0;
-        //start at 1 to skip header row
         for (let i = 1; i < (rows.length - 1); i++) {
 
             x = rows[i].querySelectorAll("td")[c];
             average += Number(x.innerHTML);
             n++;
         }
-        average = average/n;
-        td =  document.createElement('td')
-        if (c == 5) {
-            td.innerHTML = average.toFixed(0);
-            averageRow.appendChild(td);  
-        } else {
-            td.innerHTML = `$${average.toFixed(4)}`;
-            averageRow.appendChild(td);
-        }
+        average = (average/n).toFixed(3);
+        td =  document.createElement('td');
+        td.innerHTML  =  average;
+        averageRow.appendChild(td);
     }
 }
 
-/* loops through each row of stock data and finds the min for each column */
 function calculatingMin() {
     let  x, min;
     const table = document.querySelector("table.data");
-    const rows  =  table.rows;
     const minRow  =  document.querySelector("table.calculations #min-row");
     minRow.innerHTML =  "";
-    //adds Title of row to front
     let td =  document.createElement('td');
     td.innerHTML  = "<b>Min:<b>";
     minRow.appendChild(td);
-    //start at 1 to skip date column
+    const rows  =  table.rows;
+
     for (let c = 1;  c  < 6;  c++)  {
         //start min as first row entry
         min  = Number(rows[c].querySelectorAll("td")[c].innerHTML);
-        //start at 1 to skip header row
         for (let i = 1; i < (rows.length - 1); i++) {
 
             x = rows[i].querySelectorAll("td")[c];
@@ -335,33 +321,24 @@ function calculatingMin() {
                 min = Number(x.innerHTML);
             }
         }
-        td =  document.createElement('td');
-        if (c == 5) {
-            td.innerHTML  =  min.toFixed(0);
-            minRow.appendChild(td);  
-        } else {
-            td.innerHTML = `$${min.toFixed(4)}`;
-            minRow.appendChild(td);
-        }
+        td = document.createElement('td');
+        td.innerHTML  =  min;
+        minRow.appendChild(td);
     }
 }
 
-/* loops through each row of stock data and finds the max for each column */
 function calculatingMax() {
     let  x, max;
     const table = document.querySelector("table.data");
-    const rows  =  table.rows;
     const maxRow  =  document.querySelector("table.calculations #max-row");
     maxRow.innerHTML =  "";
-    //adds Title of row to front
     let td =  document.createElement('td');
     td.innerHTML  = "<b>Max:<b>";
     maxRow.appendChild(td);
-    //start at 1 to skip date column
-    for (let c = 1;  c < 6;  c++)  {
-        //start max as first row entry
+    const rows  =  table.rows;
+
+    for (let c = 1;  c  < 6;  c++)  {
         max = Number(rows[c].querySelectorAll("td")[c].innerHTML);
-        //start at 1 to skip header row
         for (let i = 1; i < (rows.length - 1); i++) {
 
             x = rows[i].querySelectorAll("td")[c];
@@ -370,94 +347,306 @@ function calculatingMax() {
             }
         }
         td =  document.createElement('td');
-        if (c == 5) {
-            td.innerHTML  =  max.toFixed(0);
-            maxRow.appendChild(td);  
-        } else {
-            td.innerHTML  =  `$${max.toFixed(4)}`;
-            maxRow.appendChild(td);
-        }
+        td.innerHTML  =  max;
+        maxRow.appendChild(td);
     }
 }
 
-/* populates the description section on the second screen, with a button
-to speak the description */
-function populateDescription(company) {
-    const title = document.querySelector('.description .title');
-    title.textContent = `${company.name} (${company.symbol})`;
-    const description = document.querySelector('.company-desc');
-    description.textContent = company.description;
-    document.querySelector('.speakBtn').addEventListener('click', (e) => {
-        const utterance = new SpeechSynthesisUtterance(description.textContent);
-        speechSynthesis.speak(utterance);
-    });
+function listAvg(list) {
+	var allVals = 0.0;
+	list.forEach((i) => allVals += parseFloat(i));
+	return allVals / list.length;
 }
 
-/* checks too see if the company has financials, if not a heading is inserted instead
-of a table. If the company does, a table is populated with that information */
-function populateFinancials(company) {
-    const finacials = company.financials;
-    console.log(finacials);
-    const section =  document.querySelector('#finance-section');
-    const h1 = document.createElement('h1');
-    const companyFinance = document.querySelector(".financial-table tbody");
-    if (!finacials) {
-        document.querySelector('.financial-table').style.display = "none";
-        h1.className = "noInfo";
-        h1.innerHTML = "No Financial Information Available";
-        section.appendChild(h1);
-        setTimeout( () => { section.removeChild(h1)}, 20000);
-    } else {
-        let td1 = document.createElement('td');
-        td1.innerHTML = "<b>Year:<b>";
-        const year = document.querySelector('.year');
-        year.appendChild(td1);
-        for (let i=0; i < finacials.years.length; i++) {
-            const tdYear = document.createElement('td');
-            tdYear.textContent = finacials.years[i];
-            year.appendChild(tdYear);
-        }
-        const td2 = document.createElement('td');
-        td2.innerHTML = "<b>Revenue:<b>";
-        const revenue = document.querySelector('.revenue');
-        revenue.appendChild(td2);
-        for (let i=0; i < finacials.revenue.length; i++) {
-            const tdRevenue = document.createElement('td');
-            tdRevenue.textContent = finacials.revenue[i];
-            revenue.appendChild(tdRevenue);
-        }
-        const td3 = document.createElement('td');
-        td3.innerHTML = "<b>Earnings:<b>";
-        const earnings = document.querySelector('.earnings');
-        earnings.appendChild(td3);
-        for (let i=0; i < finacials.earnings.length; i++) {
-            const tdEarnings = document.createElement('td');
-            tdEarnings.textContent = finacials.earnings[i];
-            earnings.appendChild(tdEarnings);
-        }
-        const td4 = document.createElement('td');
-        td4.innerHTML = "<b>Assests:<b>";
-        const assets = document.querySelector('.assets');
-        assets.appendChild(td4);
-        for (let i=0; i < finacials.assets.length; i++) {
-            const tdAssests = document.createElement('td');
-            tdAssests.textContent = finacials.assets[i];
-            assets.appendChild(tdAssests);
-        }
-        const td5 = document.createElement('td');
-        td5.innerHTML = "<b>Liabilities:<b>";
-        const liabilities = document.querySelector('.liabilities');
-        liabilities.appendChild(td5);
-        for (let i=0; i < finacials.liabilities.length; i++) {
-            const tdLiabilities = document.createElement('td');
-            tdLiabilities.textContent = finacials.liabilities[i];
-            liabilities.appendChild(tdLiabilities);
-        }
-}
-}
+/* populate secondary view */
+function populateCharts() {
+	console.log(companyStock);
+	console.log(companies);
+	const opens = companyStock.map((s) => s.open);
+	const closes = companyStock.map((s) => s.close);
+	const lows = companyStock.map((s) => s.low);
+	const highs = companyStock.map((s) => s.high);
+	const opensAvg = listAvg(opens);
+	const closesAvg = listAvg(closes);
+	const lowsAvg = listAvg(lows);
+	const highsAvg = listAvg(highs);
+	const openMin = Math.min(...opens);
+	const openMax = Math.max(...opens);
+	const closeMin = Math.min(...closes);
+	const closeMax = Math.max(...closes);
+	const lowMin = Math.min(...opens);
+	const lowMax = Math.max(...opens);
+	const highMin = Math.min(...highs);
+	const highMax = Math.max(...highs);
+	const smallestOverall = Math.min([openMin, closeMin, lowMin, highMin]);
+	const largestOverall = Math.max([openMax, closeMax, lowMax, highMax]);
+	var data = [
+		[opensAvg, opensAvg, openMin, openMax],
+		[closesAvg, closesAvg, closeMin, closeMax],
+		[lowsAvg, lowsAvg, lowMin, lowMax],
+		[highsAvg, highsAvg, highMin, highMax]
+	];
+	console.log(data);
+	var option = {
+	    xAxis: {
+		data: ['open', 'close', 'low', 'high']
+	    },
+	    yAxis: {
+	    },
+	    series: [{
+		type: 'k',
+		data: data
+	    }]
+	};
+	const cEle = document.getElementById('candlechart');
+	console.log(cEle);
+	var candleChart = echarts.init(cEle);
+	candleChart.setOption(option);
 
+	// line chart
+	// https://echarts.apache.org/examples/en/editor.html?c=bar-label-rotation
+	const lineChart = document.getElementById('barchart');
+	var app = {};
+var posList = [
+    'left', 'right', 'top', 'bottom',
+    'inside',
+    'insideTop', 'insideLeft', 'insideRight', 'insideBottom',
+    'insideTopLeft', 'insideTopRight', 'insideBottomLeft', 'insideBottomRight'
+];
+	
+app.configParameters = {
+    rotate: {
+        min: -90,
+        max: 90
+    },
+    align: {
+        options: {
+            left: 'left',
+            center: 'center',
+            right: 'right'
+        }
+    },
+    verticalAlign: {
+        options: {
+            top: 'top',
+            middle: 'middle',
+            bottom: 'bottom'
+        }
+    },
+    position: {
+        options: posList.reduce(function (map, pos) {
+            map[pos] = pos;
+            return map;
+        }, {})
+    },
+    distance: {
+        min: 0,
+        max: 100
+    }
+};
+	app.config = {
+    rotate: 90,
+    align: 'left',
+    verticalAlign: 'middle',
+    position: 'insideBottom',
+    distance: 15,
+    onChange: function () {
+        var labelOption = {
+            normal: {
+                rotate: app.config.rotate,
+                align: app.config.align,
+                verticalAlign: app.config.verticalAlign,
+                position: app.config.position,
+                distance: app.config.distance
+            }
+        };
+        myChart.setOption({
+            series: [{
+                label: labelOption
+            }, {
+                label: labelOption
+            }, {
+                label: labelOption
+            }, {
+                label: labelOption
+            }]
+        });
+    }
+};
+
+
+var labelOption = {
+    show: true,
+    position: app.config.position,
+    distance: app.config.distance,
+    align: app.config.align,
+    verticalAlign: app.config.verticalAlign,
+    rotate: app.config.rotate,
+    formatter: '{c}  {name|{a}}',
+    fontSize: 16,
+    rich: {
+        name: {
+        }
+    }
+};	
+	// TODO: VERY BAD PRACTICE!!!! DEPENDS ON GLOBAL VARIABLED BEING SET A CERTAIN WAY!
+	const companySymbol = companyStock[0].symbol;
+	const company = companies.filter((c) => c.symbol === companySymbol)[0];
+	console.log(company);
+	try {
+		let f = company.financials;
+		let seriesTemplateObject = {
+			name: '', // SET NAME
+			type: 'bar',
+			label: labelOption,
+			barGap: 0,
+			emphasis: { focus: 'series' },
+			data: [] // SET DATA
+		};
+		var years = f.years;
+		var series = [];
+
+		// a deep copy of the template is made, then slight modifications are added before pushing to series list.
+		let revSeries = JSON.parse(JSON.stringify(seriesTemplateObject));
+		revSeries.name = 'Revenue';
+		revSeries.data = f.revenue;
+		let earnSeries = JSON.parse(JSON.stringify(seriesTemplateObject));
+		earnSeries.name = 'Earnings';
+		earnSeries.data = f.earnings;
+		let assetsSeries = JSON.parse(JSON.stringify(seriesTemplateObject));
+		assetsSeries.name = 'Assets';
+		assetsSeries.data = f.assets;
+		let liabSeries = JSON.parse(JSON.stringify(seriesTemplateObject));
+		liabSeries.name = 'Liabilities';
+		liabSeries.data = f.liabilities;
+		
+		series.push(revSeries);
+		series.push(earnSeries);
+		series.push(assetsSeries);
+		series.push(liabSeries);
+
+	} catch {
+		lineChart.textContent = 'This company has no financials data.';
+	}
+	const barChart = echarts.init(document.getElementById('barchart'));
+	const barOption = {
+		legend: {
+			data: ['Revenue', 'Earnings', 'Assets', 'Liabilities']
+		},
+		toolbox: {
+			show: true,
+			orient: 'vertical',
+			left: 'right',
+			top: 'center',
+		},
+	    xAxis: [
+		{
+		    type: 'category',
+		    axisTick: {show: false},
+		    data: years
+		}
+	    ],
+	    yAxis: [
+		{
+		    type: 'value'
+		}
+	    ],
+	    series: series
+	};
+		
+	barChart.setOption(barOption);
+
+	// line chart
+	// https://echarts.apache.org/examples/en/editor.html?c=multiple-y-axis
+	const lineChartEle = document.getElementById('linechart');
+
+	const volumeData = companyStock.map((s) => s.volume);
+	const closeData = companyStock.map((s) => s.close);
+	const lowestVolume = Math.min(...volumeData);
+	const lowestClose = Math.min(...closeData);
+	const highestVolume = Math.max(...volumeData);
+	const highestClose = Math.max(...closeData);
+
+var colors = ['#5470C6', '#91CC75'];
+
+const lineOption = {
+    color: colors,
+
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'cross'
+        }
+    },
+    grid: {
+        right: '20%'
+    },
+    legend: {
+        data: ['Close Value', 'Volume']
+    },
+    xAxis: [
+        {
+            type: 'category',
+            axisTick: {
+                alignWithLabel: true
+            },
+            //data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
+        }
+    ],
+    yAxis: [
+        {
+            type: 'value',
+            name: 'Close Value',
+            min: (lowestClose * 0.8),
+            max: (highestClose * 1.2),
+            position: 'left',
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: colors[0]
+                }
+            },
+            axisLabel: {
+                formatter: '${value}/share'
+            }
+        },
+        {
+            type: 'value',
+            name: 'Volume',
+            min: (lowestVolume * 0.8),
+            max: (highestVolume * 1.2),
+            position: 'right',
+            axisLine: {
+                show: true,
+                lineStyle: {
+                    color: colors[1]
+                }
+            },
+            axisLabel: {
+                formatter: '${value}'
+            }
+        }
+    ],
+    series: [
+        {
+            name: 'Close Value',
+            type: 'line',
+            data: closeData
+        },
+        {
+            name: 'Volume',
+            type: 'line',
+            yAxisIndex: 1,
+            data: volumeData
+        },
+    ]
+};
+	const lineChartMultiY = echarts.init(lineChartEle);
+	lineChartMultiY.setOption(lineOption);
+}
 
 /* button to switch UI to secondary */
+
 document.querySelector(".chartsBtn").addEventListener('click', () => {
     document.querySelectorAll('.main').forEach( section => {
         section.style.display = 'none';
@@ -465,7 +654,7 @@ document.querySelector(".chartsBtn").addEventListener('click', () => {
     document.querySelectorAll('.second').forEach( section => {
         section.style.display = 'block';
     });
-
+    populateCharts();
  });
 
   /* button to switch UI to deafult */
